924bbe1fa02459b7b6b06ecba3c77d4def9e248c
Vinicius Dias <vinicius-mdias@hotmail.com>
2024-05-13 10:45:16 -0300

    Updates

---
+++ 5e2a4920-43830d7a92746f5a0e61.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{Tgqd:function(n,t,o){"use strict";o.d(t,"a",(function(){return i}));var e=o("Lnxd"),i=function(n){return Object(e.a)({tag:"svg",attr:{viewBox:"0 0 24 24",fill:"none",stroke:"currentColor",strokeWidth:"2",strokeLinecap:"round",strokeLinejoin:"round"},child:[{tag:"polyline",attr:{points:"9 18 15 12 9 6"}}]})(n)};i.displayName="FiChevronRight"}}]);
+//# sourceMappingURL=5e2a4920-43830d7a92746f5a0e61.js.map
