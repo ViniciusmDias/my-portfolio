9e885504769362d31057d4b4a92b8be7a1762f74
Vinicius Dias <vinicius-mdias@hotmail.com>
2024-07-12 11:14:18 -0300

    feat: add google analytics and google tag.

--- gatsby-config.js
+++ gatsby-config.js
@@ -65,9 +65,62 @@ module.exports = {
 		`gatsby-plugin-sharp`,
 		`gatsby-plugin-sitemap`,
 		{
-			resolve: `gatsby-plugin-google-analytics`,
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-9VMMMP0MN9", // Google Analytics / GA
+          // "AW-CONVERSION_ID", // Google Ads / Adwords / AW
+          // "DC-FLOODIGHT_ID",
+					"GTM-N4GKBK8Z" // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        // gtagConfig: {
+        //   optimize_id: "OPT_CONTAINER_ID",
+        //   anonymize_ip: true,
+        //   cookie_expires: 0,
+        // },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+          // Setting this parameter is also optional
+          respectDNT: true,
+          // Avoids sending pageview hits from custom paths
+          exclude: ["/preview/**", "/do-not-track/me/too/"],
+          // Defaults to https://www.googletagmanager.com
+          origin: "https://www.googletagmanager.com",
+          // Delays processing pageview events on route update (in milliseconds)
+          delayOnRouteUpdate: 0,
+        },
+      },
+    },
+		{
+			resolve: "gatsby-plugin-google-tagmanager",
 			options: {
-				trackingId: `G-9VMMMP0MN9`,
+				id: "GTM-N4GKBK8Z",
+	
+				// Include GTM in development.
+				//
+				// Defaults to false meaning GTM will only be loaded in production.
+				includeInDevelopment: false,
+	
+				// datalayer to be set before GTM is loaded
+				// should be an object or a function that is executed in the browser
+				//
+				// Defaults to null
+				defaultDataLayer: { platform: "gatsby" },
+	
+				// Name of the event that is triggered
+				// on every Gatsby route change.
+				//
+				// Defaults to false
+				enableWebVitalsTracking: true,
+				// Defaults to https://www.googletagmanager.com
+				selfHostedOrigin: "https://www.googletagmanager.com",
+				// Defaults to gtm.js
+				selfHostedPath: "/gtag/js?id=G-9VMMMP0MN9",
 			},
 		},
 		{
