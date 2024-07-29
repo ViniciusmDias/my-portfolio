589376ded48b5b3e7dae2ed76b01841a23fb0201
Vinicius Dias <vinicius-mdias@hotmail.com>
2024-07-29 17:05:13 -0300

    fix: Fix next js form problem with netlify.

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,6 +1,36 @@
+'use client'
+
+import { useState } from 'react';
 import * as S from './styles'
 
 export default function Contact() {
+	const [status, setStatus] = useState("");
+	const [error, setError] = useState("");
+
+	const handleFormSubmit = async (event: any) => {
+		event.preventDefault();
+		try {
+			setStatus('pending');
+			setError("");
+			const myForm = event.target;
+			const formData: any = new FormData(myForm);
+			const res = await fetch('/__forms.html', {
+				method: 'POST',
+				headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+				body: new URLSearchParams(formData).toString()
+			});
+			if (res.status === 200) {
+				setStatus('ok');
+			} else {
+				setStatus('error');
+				setError(`${res.status} ${res.statusText}`);
+			}
+		} catch (e) {
+				setStatus('error');
+				setError(`${e}`);
+		}
+	};
+
   return (
     <S.Contact className="container">
 			<h1
@@ -16,9 +46,7 @@ export default function Contact() {
 			<form
 				id="form"
 				name="contact"
-				method="post"
-				data-netlify="true"
-				data-netlify-honeypot="bot-field"
+				onSubmit={handleFormSubmit}
 			>
 				<input type="hidden" name="form-name" value="contact" />
 				<S.Row>
@@ -65,8 +93,58 @@ export default function Contact() {
 						/>
 					</label>
 				</S.Row>
-				<button type="submit">Send</button>
+				<button type="submit" disabled={status === 'pending'}>
+					Send
+				</button>
+				{status === 'ok' && (
+					<div className="alert alert-success">
+						<SuccessIcon />
+						Submitted!
+					</div>
+				)}
+				{status === 'error' && (
+					<div className="alert alert-error">
+						<ErrorIcon />
+						{error}
+					</div>
+				)}
 			</form>
 		</S.Contact>
   )
+}
+
+function SuccessIcon() {
+	return (
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			className="stroke-current shrink-0 h-6 w-6"
+			fill="none"
+			viewBox="0 0 24 24"
+		>
+			<path
+				strokeLinecap="round"
+				strokeLinejoin="round"
+				strokeWidth="2"
+				d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+			/>
+		</svg>
+	);
+}
+
+function ErrorIcon() {
+	return (
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			className="stroke-current shrink-0 h-6 w-6"
+			fill="none"
+			viewBox="0 0 24 24"
+		>
+			<path
+				strokeLinecap="round"
+				strokeLinejoin="round"
+				strokeWidth="2"
+				d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+			/>
+		</svg>
+	);
 }
