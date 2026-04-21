5302415f47473776c9fdffc9cc5a087e74459def
Vinicius M Dias <vinicius-mdias@hotmail.com>
2026-04-21 20:16:41 -0300

    chore: remove legacy v1 codebase
    Wipe Next 14 + styled-components app ahead of v2 rewrite.
    Drops axios, react-icons, react-hamburger-menu, styled-components.

--- app/services/api.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios"
-
-const api = axios.create({
-	baseURL: "https://api.github.com/users/ViniciusmDias",
-})
-const apiDev = axios.create({
-	baseURL: "https://dev.to/api/articles?username=viniciusmdias",
-})
-export { api, apiDev }
